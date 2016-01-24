e9609e4df3294b9a2b6fb65a970ed3808bfd4535
Tomasz Belina <tomasz.belina@qualent.eu>
2016-01-24 09:15:29 +0100

    Strumienie

--- src/main/java/stream/Reducing.java
+++ src/main/java/stream/Reducing.java
@@ -6,13 +6,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.stream.Collectors.reducing;
 import static stream.model.Dish.menu;
 
 public class Reducing{
 
-    public static void main(String...args){
+    public static void main(String...args) {
 
-        List<Integer> numbers = Arrays.asList(3,4,5,1,2);
+        List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
         int sum = numbers.stream().reduce(0, (a, b) -> a + b);
         System.out.println(sum);
 
@@ -26,8 +27,34 @@ public class Reducing{
         min.ifPresent(System.out::println);
 
         int calories = menu.stream()
-                           .map(Dish::getCalories)
-                           .reduce(0, Integer::sum);
+                .map(Dish::getCalories)
+                .reduce(0, Integer::sum);
         System.out.println("Number of calories:" + calories);
+
+        System.out.println("--------------------------------'");
+
+
+        System.out.println("Total calories in menu: " + calculateTotalCalories());
+        System.out.println("Total calories in menu: " + calculateTotalCaloriesWithMethodReference());
+        System.out.println("Total calories in menu: " + calculateTotalCaloriesWithoutCollectors());
+        System.out.println("Total calories in menu: " + calculateTotalCaloriesUsingSum());
+
+    }
+
+    private static int calculateTotalCalories() {
+        return menu.stream().collect(reducing(0, Dish::getCalories, (Integer i, Integer j) -> i + j));
+    }
+
+    private static int calculateTotalCaloriesWithMethodReference() {
+        return menu.stream().collect(reducing(0, Dish::getCalories, Integer::sum));
+    }
+
+    private static int calculateTotalCaloriesWithoutCollectors() {
+        return menu.stream().map(Dish::getCalories).reduce(Integer::sum).get();
+    }
+
+    private static int calculateTotalCaloriesUsingSum() {
+
+        return menu.stream().mapToInt(Dish::getCalories).sum();
     }
 }
