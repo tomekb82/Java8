1c8d46c86ffd1bddab8378cd528cd9b8301a7143
Tomasz Belina <tomasz.belina@qualent.eu>
2016-01-25 23:53:56 +0100

    Dodaj Optional

---
+++ src/main/java/optional/Insurance.java
@@ -0,0 +1,10 @@
+package optional;
+
+public class Insurance {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/optional/Person.java
@@ -0,0 +1,12 @@
+package optional;
+
+import java.util.*;
+
+public class Person {
+
+    private Optional<Car> car;
+
+    public Optional<Car> getCar() {
+        return car;
+    }
+}
