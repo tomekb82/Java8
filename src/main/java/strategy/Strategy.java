f4dfc341f1904f90e27a0ff9ceb639589eb2d5b4
Tomasz Belina <tomasz.belina@qualent.eu>
2016-07-16 23:00:16 +0200

    dodaj klase uzytkowa przekazuja funkcje

--- src/main/java/strategy/Strategy.java
+++ src/main/java/strategy/Strategy.java
@@ -39,8 +39,22 @@ public class Strategy {
 
         List<Integer> values = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         System.out.println(totalValues(values, e->true));
-        System.out.println(totalValues(values, e-> e  % 2 == 0 ));
-        System.out.println(totalValues(values, e-> e  % 2 != 0 ));
+        System.out.println(totalValues(values, Util::isEven));
+        System.out.println(totalValues(values, Util::isOdd));
 
     }
 }
+
+class Util {
+
+    public static boolean isEven(int number){
+        return number % 2 == 0;
+    }
+
+    public static boolean isOdd(int number){
+        return number % 2 != 0;
+    }
+}
+
+
+
